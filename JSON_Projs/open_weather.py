87a1ca00cbf40a172833ed6148c3879f2729cb9c
Xmexy <azaman@browning.edu>
2021-08-20 17:42:52 -0400

    .csv files created from excel_to_csv project

--- JSON_Projs/open_weather.py
+++ JSON_Projs/open_weather.py
@@ -1,22 +1,30 @@
 #! python3
-# open_weather.py - Prints the weather for a location from the command line.
+# Prints the current weather for a location from the command line.
 
 import json, requests, sys
 
-APP_ID = 'Insert_ID_Here'
+APPID = 'a667f2396d0a0f1eb4cf59255ec0ea1b'
 
-
-# Computer location from command line arguments.
+# Compute location from command line arguments
 if len(sys.argv) < 2:
-    print('Usage: open_weather.py city_name, 2-letter_country_code')
+    print('Usage: quickweather.py city_name, 2-letter_country_code')
     sys.exit()
-
 location = ' '.join(sys.argv[1:])
 
-# Download the JSON data from OpenWeatherMap.org's API.
-url = f'https://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3&APPID=%s ' % (location, APP_ID)
-
+# Download the JSON data from openweathermap.org's API
+url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3&APPID=%s' % (location, APPID)
 response = requests.get(url)
 response.raise_for_status()
 
-print(response.text)
+# Load JSON data into Python variable.
+weatherData = json.loads(response.text)
+
+w = weatherData['list']
+print(f'Current weather in {location}:')
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
